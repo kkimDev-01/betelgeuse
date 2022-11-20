ecfbc3d1e09a9e83db758781049ee13afdb2ce6a
kkimDev-01 <kkheentech@gmail.com>
2022-11-20 11:08:20 +0900

    first commit

---
+++ src/main/java/com/example/betelgeuse/BetelgeuseApplication.java
@@ -0,0 +1,13 @@
+package com.example.betelgeuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetelgeuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetelgeuseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/betelgeuse/BetelgeuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betelgeuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetelgeuseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
